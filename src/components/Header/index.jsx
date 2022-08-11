f7c52549bd8f0af211014e0cd1b6d79dc6b5faf8
Denis Repyev <sinedviper@gmail.com>
2022-08-11 17:53:47 +0300

    bag fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -35,7 +35,7 @@ export const Header = () => {
                   variant="contained"
                   color="error"
                 >
-                  Выйти
+                  Exit
                 </Button>
               </>
             ) : (
